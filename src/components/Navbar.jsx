23d63e3d4f5be4f0eb19c254d504da72120b0450
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-10 00:51:49 +0530

    Made navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,6 +37,18 @@ const Navbar = () => {
                     </button>
                 </div>
             </div>
+            {mobileDrawer && (
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                    <ul>
+                        <li>
+                            <a href="#">Features</a>
+                        </li>
+                        <li>
+                            <a href="#"></a>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     </nav>
   )
