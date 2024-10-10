870fb02502403bd82fb2adacaf4f9ffdbb9f96d7
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-10 10:37:18 +0530

    Added description and buttons on hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,6 +9,19 @@ const HeroSection = () => {
                 className='bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text'
             >{" "}for developers</span>
         </h1>
+        <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+            Empower your creativity and bring your VR app ideas to life with our
+            intuitive development tools. Get started today and turn your imagination into
+            immersive reality
+        </p>
+        <div className='flex justify-center my-10'>
+            <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 rounded-md'>
+                Start for free
+            </a>
+            <a href="#" className='py-3 px-4 mx-3 rounded-md border'>
+                Documentation
+            </a>
+        </div>
     </div>
   )
 }
