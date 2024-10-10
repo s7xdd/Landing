90013f7f7848745cd4aacf499385f2896f2e3e27
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-10 11:53:28 +0530

    Added videos in hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
 
 const HeroSection = () => {
   return (
@@ -21,6 +23,27 @@ const HeroSection = () => {
             <a href="#" className='py-3 px-4 mx-3 rounded-md border'>
                 Documentation
             </a>
+        </div>
+        <div className='flex mt-2 justify-center pl-14 pr-14'>
+            <video 
+                autoPlay 
+                loop 
+                muted
+                className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'
+                >
+                    <source src={video1} type='video/mp4'/>
+                    Your browser does not support the video tag
+            </video>
+
+            <video 
+                autoPlay 
+                loop 
+                muted
+                className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'
+                >
+                    <source src={video2} type='video/mp4'/>
+                    Your browser does not support the video tag
+            </video>
         </div>
     </div>
   )
