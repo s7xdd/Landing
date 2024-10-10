a11a97c52e458047539003c82988312da413cff0
Mohammed Shamil <kmshamil123@gmail.com>
2024-10-10 14:16:24 +0530

    Added workflow section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
     <div className="main">
       <Navbar />
-      <div className="max-w-7xl mx-auto  px-6">
+      <div className="max-w-7xl mx-auto px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
       </div>
     </div>
   );
--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -8,7 +8,7 @@ import { GlobeLock } from "lucide-react";
 
 const FeatureSection = () => {
   return (
-    <div className="mt-20 border-b border-neutral-800 min-h-[800px]">
+    <div className="mt-20 border-b border-neutral-800 min-h-[750px]">
       <div className="text-center">
         <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
           feature
